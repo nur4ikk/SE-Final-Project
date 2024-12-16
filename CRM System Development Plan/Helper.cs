51aa08a8df3b57ca6f1191c27f61bd5fd006e347
Nursultan Kurmanaliev <nursultankurmanaliev25@gmail.com>
2024-12-16 21:48:06 +0600

    Helpers added

---
+++ CRM System Development Plan/Helper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM_System_Development_Plan
+{
+    public class Helper
+    {
+
+        public static bool IsAdmin;
+     
+        // Use Application Base Directory for Consistent File Placement
+        public static string connectionString =
+            $"Data Source={AppDomain.CurrentDomain.BaseDirectory}CRMSystemDevelopmentPlan.db;Version=3;";
+    }
+}
